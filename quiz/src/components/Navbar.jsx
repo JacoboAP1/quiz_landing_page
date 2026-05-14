ff6d5087ade9c522cb69b6ae99a756f729c98cc7
JacoboAP1 <jacobo.arroyavep@autonoma.edu.co>
2026-05-14 11:06:40 -0500

    Uploading base html code for components and app working

---
+++ quiz/src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <section className="h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-3xl">Espacio para el Navbar</h1>
+    </section>
+  );
+}
