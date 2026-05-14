edab84edf1a5c740359a5067cb5e391560b10832
JacoboAP1 <jacobo.arroyavep@autonoma.edu.co>
2026-05-14 10:48:44 -0500

    Uploading base react + tailwind css base

---
+++ quiz/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-red-600">
+      <h1 className="text-5xl font-bold text-white drop-shadow-lg">
+        ¡Tailwind funciona! 
+      </h1>
+    </div>
+  )
+}
+
+export default App
